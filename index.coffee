36a4c1453dc0ae259886ca749265ffc2c3980ca6
Alicia Sykes <sykes.alicia@gmail.com>
2015-10-16 16:12:42 +0100

    Wrote everything

---
+++ index.coffee
@@ -0,0 +1,32 @@
+request = require 'request'
+querystring = require 'querystring'
+
+host = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+
+
+makeURL = (paramaters, apiKey)->
+  urlParams =
+    if typeof paramaters is 'string' then 'query='+ paramaters
+    else if typeof paramaters is 'object' then querystring.stringify(paramaters)
+  host + '?' + 'key=' + apiKey + '&' + urlParams
+
+
+formatResults = (body)->
+  place_name:  body.results[0]['formatted_address']
+  location: body.results[0]['geometry']['location']
+
+
+makeRequest = (url, requestCallback)->
+  request { url: url, json: true},
+  (error, response, body) ->
+    if !error and response.statusCode == 200 then requestCallback(body)
+
+
+main = (paramaters, apiKey, callback)->
+  url = makeURL(paramaters, apiKey)
+  makeRequest(url, (results)->
+    try callback(formatResults(results))
+    catch e then callback({error: 'Zero results returned'})
+  )
+
+module.exports = main
---
+++ index.js
@@ -0,0 +1,54 @@
+(function() {
+  var formatResults, host, main, makeRequest, makeURL, querystring, request;
+
+  request = require('request');
+
+  querystring = require('querystring');
+
+  host = "https://maps.googleapis.com/maps/api/place/textsearch/json";
+
+  makeURL = function(paramaters, apiKey) {
+    var urlParams;
+    urlParams = typeof paramaters === 'string' ? 'query=' + paramaters : typeof paramaters === 'object' ? querystring.stringify(paramaters) : void 0;
+    return host + '?' + 'key=' + apiKey + '&' + urlParams;
+  };
+
+  formatResults = function(body) {
+    return {
+      place_name: body.results[0]['formatted_address'],
+      location: body.results[0]['geometry']['location']
+    };
+  };
+
+  makeRequest = function(url, requestCallback) {
+    return request({
+      url: url,
+      json: true
+    }, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        return requestCallback(body);
+      }
+    });
+  };
+
+  main = function(paramaters, apiKey, callback) {
+    var url;
+    url = makeURL(paramaters, apiKey);
+    return makeRequest(url, function(results) {
+      var e, error1;
+      try {
+        return callback(formatResults(results));
+      } catch (error1) {
+        e = error1;
+        return callback({
+          error: 'Zero results returned'
+        });
+      }
+    });
+  };
+
+  module.exports = main;
+
+}).call(this);
+/* (C) Alicia Sykes <alicia@aliciasykes.com> 2015           *\
+\* MIT License. Read full license at: https://goo.gl/IL4lQJ */
